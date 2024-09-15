a084d05ce87531bc270aa4e32e621a120392b324
FelipeAlbarelli <felipesalbarelli@gmail.com>
2024-09-15 10:58:25 -0300

    (board) feat: spawn item on board on drop

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { DropAreaDirective } from '../../drag-drop/drop-area.directive';
-import { DragDropData, DropCompletedDataInfo, DropInfo } from '../../drag-drop/drag-drop-service.service';
+import { Coord, DragDropData, DropCompletedDataInfo, DropInfo } from '../../drag-drop/drag-drop-service.service';
+
+type ItemsToDisplay = {
+  coord : Coord,
+  name : string
+}
 
 @Component({
   selector: 'app-board',
@@ -11,10 +16,18 @@ import { DragDropData, DropCompletedDataInfo, DropInfo } from '../../drag-drop/d
 })
 export class BoardComponent {
 
-  log = (e: any, tag?: string) => {
+  items : ItemsToDisplay[] = []
+
+  log = (e: any) => {
+    console.log(e)
   }
 
   onDrop = (data : DropCompletedDataInfo) => {
-    console.log(data);
+    const {dropInfo , dragData} = data;
+    console.log(dragData)
+    this.items.push({
+      coord : dropInfo.coord.dict.page,
+      name : `${dragData['name']}` ?? '---'
+    })
   }
 }
